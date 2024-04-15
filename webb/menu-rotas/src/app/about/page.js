b5424d05940e176347abdf30aa5431fee5248807
Letícia Santiago da Silva <101338974+LehSantiago@users.noreply.github.com>
2024-04-15 10:51:31 -0300

    Add files via upload

---
+++ webb/menu-rotas/src/app/about/page.js
@@ -0,0 +1,10 @@
+import Menu from '@/componentes/menu';
+//import styles from './page.modules.css'
+
+export default function About(){
+  return(
+    <>
+        <Menu />
+    </>
+  );
+}
---
+++ webb/menu-rotas/src/app/contact/page.js
@@ -0,0 +1,10 @@
+import Menu from '@/componentes/menu';
+import styles from './contact.modules.css'
+
+export default function Contact(){
+  return(
+    <>
+        <Menu/>
+    </>
+  );
+}
